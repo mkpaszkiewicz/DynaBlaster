cc777e9b955b02811567c7f5104bbf38e283db6a
Marcin Paszkiewicz <mkpaszkiewicz@gmail.com>
2015-10-10 21:13:51 +0200

    Added new sprite sheets factory methods

--- src/main/java/paszkiewicz/marcin/util/SpriteSheets.java
+++ src/main/java/paszkiewicz/marcin/util/SpriteSheets.java
@@ -7,4 +7,8 @@ import paszkiewicz.marcin.util.factory.SlickFactory;
 public class SpriteSheets
 {
     public static SpriteSheet basicSpriteSheet = SlickFactory.createSpriteSheet("graphics/basic_board.png", 16, 16, 1);
+    
+    public static SpriteSheet playerSpriteSheet = SlickFactory.createSpriteSheet("graphics/player_animation.png", 23, 23, 1);
+    
+    public static SpriteSheet monsterSpriteSheet = SlickFactory.createSpriteSheet("graphics/monsters_animation.png", 16, 20, 0);
 }
