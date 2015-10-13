c33b9693f01b265b53dec4beb41dcbd4d5539f92
Marcin Paszkiewicz <mkpaszkiewicz@gmail.com>
2015-10-14 01:52:56 +0200

    Change view into fullscreen

--- game/src/main/java/paszkiewicz/marcin/core/impl/GameCoreImpl.java
+++ game/src/main/java/paszkiewicz/marcin/core/impl/GameCoreImpl.java
@@ -44,10 +44,9 @@ public class GameCoreImpl implements GameCore
         int screenWidth = Display.getDisplayMode().getWidth();
         int screenHeight = Display.getDisplayMode().getHeight();
 
-        //application = new ApplicationContainer(this.gameModel, screenWidth, screenHeight, true);
-        application = new ApplicationContainer(this.gameModel, 600, 600, false);
+        application = new ApplicationContainer(this.gameModel, screenWidth, screenHeight, true);
         application.setShowFPS(false);
-        //application.setMouseGrabbed(true);
+        application.setMouseGrabbed(true);
         application.setAlwaysRender(true);
     }
 
